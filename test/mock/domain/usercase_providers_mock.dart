55e3acaa6684c63b821d86817aa8c3d60dea6633
Vinícius Corrêa de Almeida <>
2024-03-31 12:53:13 -0300

    chore: delete unsed files

--- test/mock/domain/usercase_providers_mock.dart
+++
@@ -1,6 +0,0 @@
-import 'package:coolmovies/domain/usecases/get_users_usecase.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mockito/annotations.dart';
-
-@GenerateMocks([Provider<GetUsersUsecase>])
-void main() {}
