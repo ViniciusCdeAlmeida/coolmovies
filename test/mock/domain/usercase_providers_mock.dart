7538e70672e9a947eafbede8cdee85bf13a169a4
Vinícius Corrêa de Almeida <>
2024-03-31 12:57:07 -0300

    Revert "chore: delete unsed files"
    This reverts commit 55e3acaa6684c63b821d86817aa8c3d60dea6633.

---
+++ test/mock/domain/usercase_providers_mock.dart
@@ -0,0 +1,6 @@
+import 'package:coolmovies/domain/usecases/get_users_usecase.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([Provider<GetUsersUsecase>])
+void main() {}
